fc1ae0b848a3b3d510a9870ff70ebe90fcfe0f9b
Alix von Schirp <github@avonschirp.bootmedia.de>
2025-03-10 21:43:05 +0100

    chore: update eslint config
    Ignore build folders, configs as well as dotfiles and dotfolders

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { ignores: ["dist/**", "docs/**", ".*/**", "**/.*", "**/*.config.*"] },
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
