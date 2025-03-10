c38017296069453f1a650476741910485c5fb696
Alix von Schirp <github@avonschirp.bootmedia.de>
2025-03-10 07:35:56 +0100

    chore: configure typedoc

---
+++ typedoc.config.mjs
@@ -0,0 +1,28 @@
+// @ts-nocheck
+/** @type {import("typedoc").TypeDocOptions &
+ * import("typedoc-plugin-extras").ExtrasOptions &
+ * import("typedoc-plugin-coverage").CoverageOptions &
+ * import("typedoc-plugin-mdn-links").MdnLinksOptions &
+ * import("typedoc-plugin-zod").ZodOptions} */
+const config = {
+  compilerOptions: {
+    skipLibCheck: true,
+    strict: false,
+  },
+  entryPoints: ["src/index.ts"],
+  out: "docs",
+  plugin: [
+    "typedoc-plugin-coverage",
+    "typedoc-plugin-extras",
+    "typedoc-plugin-inline-sources",
+    "typedoc-plugin-mdn-links",
+    "typedoc-plugin-zod",
+  ],
+  footerTypedocVersion: true,
+  footerLastModified: true,
+  coverageLabel: "Docs Coverage",
+  coverageOutputType: "all",
+  coverageSvgWidth: 130,
+};
+
+export default config;
