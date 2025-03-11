8f11bdc43ee8eb6a855adc2b86637479a5a78ab2
Alix von Schirp <github@avonschirp.bootmedia.de>
2025-03-12 00:52:08 +0100

    ci(docs): Added theme to docs generator

--- typedoc.config.mjs
+++ typedoc.config.mjs
@@ -19,12 +19,11 @@ const config = {
     "typedoc-plugin-zod",
     "typedoc-plugin-include-example",
     "@shipgirl/typedoc-plugin-versions",
+    "typedoc-github-theme",
   ],
-  footerTypedocVersion: true,
-  footerLastModified: true,
-  coverageLabel: "Docs Coverage",
-  coverageOutputType: "all",
-  coverageSvgWidth: 130,
+  customFooterHtml: '<p class="tsd-generator" style="display: inline-flex; flex-direction: row; justify-content: space-around; width: 100%;"> <span> Made with ❤ by <a href="https://b00tload.space">Alix von Schirp</a> @ <a href="https://github.com/cis-oss">CISLabs OSS</a> </span> <span> <a href="https://github.com/cis-oss/pushover" target="_blank">GitHub</a> | <a href="https://github.com/cis-oss/pushover/issues" target="_blank">Issues</a> | <a href="https://github.com/cis-oss/pushover/blob/main/LICENSE" target="_blank">License</a> </span> <span>Generated using <a href="https://typedoc.org/" target="_blank">TypeDoc </a> with <a href="https://github.com/JulianWowra/typedoc-github-theme" target="_blank">typedoc-github-theme</a></span></p>',
+  coverageOutputType: "json",
+  hideGenerator: true,
 };
 
 export default config;
