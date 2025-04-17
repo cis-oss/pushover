6d2743e7e840d815aea671d22aa24781164bfe22
Alix von Schirp <github@avonschirp.bootmedia.de>
2025-04-17 04:54:51 +0200

    feat: exports types
    Signed-off-by: Alix von Schirp <github@avonschirp.bootmedia.de>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 import { Pushover } from "./Pushover";
 
+import type {
+  PushoverRecipient,
+  PushoverMessage,
+  PushoverMessageResponse,
+  PushoverValidationResponse,
+  PushoverReceiptResponse,
+  PushoverTagCancellationResponse,
+  ValidateOptions,
+  SendOptions,
+} from "./Pushover";
+
 export default Pushover;
+
+export type {
+  PushoverRecipient,
+  PushoverMessage,
+  PushoverMessageResponse,
+  PushoverValidationResponse,
+  PushoverReceiptResponse,
+  PushoverTagCancellationResponse,
+  ValidateOptions,
+  SendOptions,
+};
